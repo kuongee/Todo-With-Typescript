0f9fc4b375b0a580bcd942c343cf7ed7c07150b2
Jeesoo Min <jeesoo.min@samsung.com>
2020-03-26 20:54:45 +0900

    Add complete state and setState function in TodoList component

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -1,16 +1,28 @@
 interface Item {
-    content: string;
+  content: string;
+  isComplete: boolean;
 }
 
 export default class TodoList {
-    todoItems: Array<Item>;
+  todoItems: Array<Item>;
 
-    constructor(items: Array<Item>) {
-        this.todoItems = items;
-        this.render();
-    }
+  constructor(items: Array<Item>) {
+    this.todoItems = items;
+    this.render();
+  }
 
-    render() {
-        document.querySelector("#todo-list").innerHTML = this.todoItems.map(item => `<div> ☆${item.content} </div>`).join("");
-    }
+  render() {
+    document.querySelector("#todo-list").innerHTML = this.todoItems
+      .map(item =>
+        item.isComplete
+          ? `<div><s> ☆${item.content} </s></div>`
+          : `<div> ☆${item.content} </div>`
+      )
+      .join("");
+  }
+
+  setState(newItems) {
+    this.todoItems = newItems;
+    this.render();
+  }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import TodoList from "./TodoList";
 
 const todoItems = [
-    {
-        content: "Typescript 공부"
-    },
-    {
-        content: "todo"
-    }
+  {
+    content: "Typescript 공부",
+    isComplete: false
+  },
+  {
+    content: "todo 만들기",
+    isComplete: false
+  }
 ];
 
 const todolist = new TodoList(todoItems);
+
+const newTodoItems = [
+  {
+    content: "Typescript 공부",
+    isComplete: false
+  },
+  {
+    content: "todo 만들기",
+    isComplete: true
+  }
+];
+
+todolist.setState(newTodoItems);
