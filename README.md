d8b8362461364eec061577072ae6ede3bfdaa29c
Jeesoo Min <kuongee@gmail.com>
2020-04-19 00:33:04 +0900

    Add webpack comments

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Todo-With-Typescript
 
+## Typescript로 구현하는 Todo 목록
+
 ```
 // 개발용
 npm run dev
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,27 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    app: './src/app.ts'
+  entry: './src/app.ts', // Entry 파일 설정
+  output: {
+    // 번들링 결과 파일 이름과 경로 지정
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
+    // 모듈과 관련된 설정
     rules: [
+      // 모듈이 어떻게 다루어질지에 대한 규칙
+      // 모듈을 해석할 로더를 정의
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    // 모듈 처리 방식을 정함 (import 또는 require로 모듈을 불러올 때)
+    extensions: ['.ts'], // 지정한 경로가 파일인 경우 파일을 어떤 확장자로 정할지 정의
   },
   devServer: {
     contentBase: __dirname,
@@ -27,6 +30,6 @@ module.exports = {
     inline: true,
     hot: true,
     host: 'localhost',
-    port: 9000
-  }
+    port: 9000,
+  },
 };
