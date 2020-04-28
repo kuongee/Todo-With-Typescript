38b5f0b9379830ea1d27576b62ad42debe2fbd6d
Jeesoo Min <kuongee@gmail.com>
2020-04-29 00:10:42 +0900

    Add TextInput storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,25 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
+  addons: ['@storybook/addon-actions/register'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.ts$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader')
+        }
+      ]
+    });
+
+    config.resolve.extensions.push('.ts');
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../assets')
+    });
+    return config;
+  }
 };
--- src/components/atoms/input/input.stories.js
+++ src/components/atoms/input/input.stories.js
@@ -1,5 +1,12 @@
 import { storiesOf } from '@storybook/html';
+import { action } from '@storybook/addon-actions';
 import TextInput from './TextInput';
 
 const stories = storiesOf('Input', module);
-stories.add('Text Input', () => TextInput({ placeholder: 'Text input' }));
+stories.add('Text Input', () =>
+  TextInput({
+    placeholder: 'Text input',
+    value: '',
+    onKeyPress: action('key press')
+  })
+);
